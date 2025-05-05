47eb44569c7feb3ee9517b61c1fffdbd447af652
daenviong <daenviong@gmail.com>
2025-05-04 23:34:11 -0500

    Update ConexionBD.java
    Actualización ssl

--- src/persistencia/ConexionBD.java
+++ src/persistencia/ConexionBD.java
@@ -5,7 +5,7 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class ConexionBD {
-    private static final String URL = "jdbc:postgresql://dpg-d0c2j01r0fns73e05ba0-a.oregon-postgres.render.com:5432/sghlu_db";
+    private static final String URL = "jdbc:postgresql://dpg-d0c2j01r0fns73e05ba0-a.oregon-postgres.render.com:5432/sghlu_db?ssl=true";
     private static final String USER = "sghlu_user";
     private static final String PASSWORD = "hURiKnDSetYGCpUrFCRUDdrTpqrzmOOD";
 
@@ -13,7 +13,7 @@ public class ConexionBD {
         try {
             return DriverManager.getConnection(URL, USER, PASSWORD);
         } catch (SQLException e) {
-            e.printStackTrace();
+            e.printStackTrace();  // <-- Muy importante
             return null;
         }
     }
